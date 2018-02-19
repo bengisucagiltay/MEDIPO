83565a8517e134f9715587326726693e0561d292
bengisucagiltay <bengisu_cagiltay@yahoo.com>
2018-02-19 10:57:26 +0300

    css classes

--- Medipo/src/register/Register.java
+++ Medipo/src/register/Register.java
@@ -11,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.Scanner;
-import org.apache.commons.validator.*;
 
 
 /**
